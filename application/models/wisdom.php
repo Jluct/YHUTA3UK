0aa56870a5e94cf5ffb30abd69ca94d1ee1601f2
Jluct <panaev02@gmail.com>
2016-02-14 20:40:04 +0300

    Problem to DBconnect

--- application/models/wisdom.php
+++ application/models/wisdom.php
@@ -11,27 +11,287 @@ class wisdom
 
     function getWisdomByType($array) //Вопрос о получении $wisdomData стаётся открытым. Пока костыль
     {
-        $wisdomType = [1=>'education',2=>'course',3=>'seminar'];
+        $wisdomTypeArray = [1 => 'education', 2 => 'course', 3 => 'seminar'];
 
-        if(count($wisdomType)<$array[0])
-            die($array[0]." - ".count($wisdomType));
+//        if (count($wisdomTypeArray) < $array[0])
+//            die($array[0] . " - " . count($wisdomTypeArray));
 
-        $data = R::getAll("SELECT * FROM wisdom_type
+        $wisdomType = R::load('wisdom_type',$array[0]);
+        $wisdomSubtype = R::findAll('wisdom_subtype','where wisdom_type_id = :id',['id'=>$wisdomType->id]);
+//        print_r($wisdomSubtype);die();
+        $wisdomType->ownWisdom_subtypeList[]=$wisdomSubtype;
+        $wisdom = R::findAll('wisdom',"where wisdom.wisdom_subtype_id =:id",['id'=>1]);
+
+
+//        print_r($wisdom);die();
+//        $wisdomSubtype->ownWisdomList[]=$wisdom;
+
+//        print_r($wisdomType->ownWisdom_subtype[1]->wisdom_subtype_name);
+        print_r($wisdomSubtype);
+        print_r($wisdom);
+
+
+    }
+}
+
+
+
+
+/*
+ * SELECT * FROM wisdom_type
         LEFT JOIN wisdom_subtype ON wisdom_subtype.wisdom_type_id = wisdom_type.id
         LEFT JOIN wisdom ON wisdom.wisdom_subtype_id = wisdom_subtype.id
         LEFT JOIN education ON  wisdom.id= education.wisdom_id
         WHERE wisdom_type.id = 1
         order by wisdom_subtype.id
-        LIMIT 0,30"
-            );
-        $wisdom = R::convertToBeans('wisdom',$data);
+        LIMIT 0,30
+ */
 
-        $out;
 
-        foreach($wisdom as $w){
-            $out[$w->wisdom_type_id].=$w;
-        }
+/*
+ *
+RedBeanPHP\OODBBean Object
+(
+    [properties:protected] => Array
+        (
+            [id] => 1
+            [wisdom_type_name] => Высшее образование
+            [ownWisdom_subtype] => Array
+                (
+                    [1] => RedBeanPHP\OODBBean Object
+                        (
+                            [properties:protected] => Array
+                                (
+                                    [id] => 1
+                                    [wisdom_type_id] => 1
+                                    [wisdom_subtype_name] => Первое высшее образование
+                                )
 
-        print_r($out);
-    }
-}
+                            [__info:protected] => Array
+                                (
+                                    [type] => wisdom_subtype
+                                    [sys.id] => id
+                                    [sys.orig] => Array
+                                        (
+                                            [id] => 1
+                                            [wisdom_type_id] => 1
+                                            [wisdom_subtype_name] => Первое высшее образование
+                                        )
+
+                                    [tainted] =>
+                                    [changed] =>
+                                    [sys.parentcache.wisdom_type] => RedBeanPHP\OODBBean Object
+ *RECURSION*
+                                )
+
+                            [beanHelper:protected] => RedBeanPHP\BeanHelper\SimpleFacadeBeanHelper Object
+                                (
+                                )
+
+                            [fetchType:protected] =>
+                            [withSql:protected] =>
+                            [withParams:protected] => Array
+                                (
+                                )
+
+                            [aliasName:protected] =>
+                            [via:protected] =>
+                            [noLoad:protected] =>
+                            [all:protected] =>
+                        )
+
+                    [2] => RedBeanPHP\OODBBean Object
+                        (
+                            [properties:protected] => Array
+                                (
+                                    [id] => 2
+                                    [wisdom_type_id] => 1
+                                    [wisdom_subtype_name] => Переподготовка
+                                )
+
+                            [__info:protected] => Array
+                                (
+                                    [type] => wisdom_subtype
+                                    [sys.id] => id
+                                    [sys.orig] => Array
+                                        (
+                                            [id] => 2
+                                            [wisdom_type_id] => 1
+                                            [wisdom_subtype_name] => Переподготовка
+                                        )
+
+                                    [tainted] =>
+                                    [changed] =>
+                                    [sys.parentcache.wisdom_type] => RedBeanPHP\OODBBean Object
+ *RECURSION*
+                                )
+
+                            [beanHelper:protected] => RedBeanPHP\BeanHelper\SimpleFacadeBeanHelper Object
+                                (
+                                )
+
+                            [fetchType:protected] =>
+                            [withSql:protected] =>
+                            [withParams:protected] => Array
+                                (
+                                )
+
+                            [aliasName:protected] =>
+                            [via:protected] =>
+                            [noLoad:protected] =>
+                            [all:protected] =>
+                        )
+
+                    [3] => RedBeanPHP\OODBBean Object
+                        (
+                            [properties:protected] => Array
+                                (
+                                    [id] => 3
+                                    [wisdom_type_id] => 1
+                                    [wisdom_subtype_name] => Сокращённое обучение
+                                )
+
+                            [__info:protected] => Array
+                                (
+                                    [type] => wisdom_subtype
+                                    [sys.id] => id
+                                    [sys.orig] => Array
+                                        (
+                                            [id] => 3
+                                            [wisdom_type_id] => 1
+                                            [wisdom_subtype_name] => Сокращённое обучение
+                                        )
+
+                                    [tainted] =>
+                                    [changed] =>
+                                    [sys.parentcache.wisdom_type] => RedBeanPHP\OODBBean Object
+ *RECURSION*
+                                )
+
+                            [beanHelper:protected] => RedBeanPHP\BeanHelper\SimpleFacadeBeanHelper Object
+                                (
+                                )
+
+                            [fetchType:protected] =>
+                            [withSql:protected] =>
+                            [withParams:protected] => Array
+                                (
+                                )
+
+                            [aliasName:protected] =>
+                            [via:protected] =>
+                            [noLoad:protected] =>
+                            [all:protected] =>
+                        )
+
+                    [4] => Array
+                        (
+                        )
+
+                )
+
+        )
+
+    [__info:protected] => Array
+        (
+            [type] => wisdom_type
+            [sys.id] => id
+            [sys.orig] => Array
+                (
+                    [id] => 1
+                    [wisdom_type_name] => Высшее образование
+                )
+
+            [tainted] => 1
+            [changed] =>
+            [sys.shadow.ownWisdom_subtype] => Array
+                (
+                    [1] => RedBeanPHP\OODBBean Object
+                        (
+                            [properties:protected] => Array
+                                (
+                                    [id] => 1
+                                    [wisdom_type_id] => 1
+                                    [wisdom_subtype_name] => Первое высшее образование
+                                )
+
+                            [__info:protected] => Array
+                                (
+                                    [type] => wisdom_subtype
+                                    [sys.id] => id
+                                    [sys.orig] => Array
+                                        (
+                                            [id] => 1
+                                            [wisdom_type_id] => 1
+                                            [wisdom_subtype_name] => Первое высшее образование
+                                        )
+
+                                    [tainted] =>
+                                    [changed] =>
+                                    [sys.parentcache.wisdom_type] => RedBeanPHP\OODBBean Object
+ *RECURSION*
+                                )
+
+                            [beanHelper:protected] => RedBeanPHP\BeanHelper\SimpleFacadeBeanHelper Object
+                                (
+                                )
+
+                            [fetchType:protected] =>
+                            [withSql:protected] =>
+                            [withParams:protected] => Array
+                                (
+                                )
+
+                            [aliasName:protected] =>
+                            [via:protected] =>
+                            [noLoad:protected] =>
+                            [all:protected] =>
+                        )
+
+                    [2] => RedBeanPHP\OODBBean Object
+                        (
+                            [properties:protected] => Array
+                                (
+                                    [id] => 2
+                                    [wisdom_type_id] => 1
+                                    [wisdom_subtype_name] => Переподготовка
+                                )
+
+                            [__info:protected] => Array
+                                (
+                                    [type] => wisdom_subtype
+                                    [sys.id] => id
+                                    [sys.orig] => Array
+                                        (
+                                            [id] => 2
+                                            [wisdom_type_id] => 1
+                                            [wisdom_subtype_name] => Переподготовка
+                                        )
+
+                                    [tainted] =>
+                                    [changed] =>
+                                    [sys.parentcache.wisdom_type] => RedBeanPHP\OODBBean Object
+ *RECURSION*
+                                )
+
+                            [beanHelper:protected] => RedBeanPHP\BeanHelper\SimpleFacadeBeanHelper Object
+                                (
+                                )
+
+                            [fetchType:protected] =>
+                            [withSql:protected] =>
+                            [withParams:protected] => Array
+                                (
+                                )
+
+                            [aliasName:protected] =>
+                            [via:protected] =>
+                            [noLoad:protected] =>
+                            [all:protected] =>
+                        )
+
+                    [3] => RedBeanPHP\OODBBean Object
+                        (
+                            [properties:protected] => Array
+  …
+ */
