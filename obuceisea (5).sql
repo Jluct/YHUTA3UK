fa649e1d01d64af33fedcabe158fbf823746c6ff
panaev02@gmail.com <panaev02@gmail.com>
2016-02-21 12:22:02 +0300

    Actual DB

---
+++ obuceisea (5).sql
@@ -0,0 +1,413 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.6
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Фев 21 2016 г., 13:21
+-- Версия сервера: 5.1.67-community-log
+-- Версия PHP: 5.4.11
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `obuceisea`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `courses`
+--
+
+CREATE TABLE IF NOT EXISTS `courses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `wisdom_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `discipline`
+--
+
+CREATE TABLE IF NOT EXISTS `discipline` (
+  `discipline_id` int(11) NOT NULL AUTO_INCREMENT,
+  `education_id` int(11) NOT NULL,
+  PRIMARY KEY (`discipline_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `education`
+--
+
+CREATE TABLE IF NOT EXISTS `education` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `wisdom_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Дамп данных таблицы `education`
+--
+
+INSERT INTO `education` (`id`, `wisdom_id`) VALUES
+(1, 1),
+(2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `lectures`
+--
+
+CREATE TABLE IF NOT EXISTS `lectures` (
+  `lectures_id` int(11) NOT NULL AUTO_INCREMENT,
+  `discipline_id` int(11) NOT NULL,
+  PRIMARY KEY (`lectures_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `lesson`
+--
+
+CREATE TABLE IF NOT EXISTS `lesson` (
+  `lesson_id` int(11) NOT NULL AUTO_INCREMENT,
+  `courses_id` int(11) NOT NULL,
+  PRIMARY KEY (`lesson_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `menu`
+--
+
+CREATE TABLE IF NOT EXISTS `menu` (
+  `menu_id` int(11) NOT NULL AUTO_INCREMENT,
+  `menu_name` char(40) NOT NULL,
+  PRIMARY KEY (`menu_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- Дамп данных таблицы `menu`
+--
+
+INSERT INTO `menu` (`menu_id`, `menu_name`) VALUES
+(1, 'Верхнее меню'),
+(2, 'Главное меню'),
+(3, 'Меню студента'),
+(4, 'Меню модератора'),
+(5, 'Меню категорий');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `menu_item`
+--
+
+CREATE TABLE IF NOT EXISTS `menu_item` (
+  `menu_item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `menu_id` int(11) NOT NULL,
+  `menu_item_name` char(80) NOT NULL,
+  `menu_item_url` char(80) NOT NULL,
+  `menu_item_parent` int(11) NOT NULL DEFAULT '0',
+  `menu_item_activ` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`menu_item_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=16 ;
+
+--
+-- Дамп данных таблицы `menu_item`
+--
+
+INSERT INTO `menu_item` (`menu_item_id`, `menu_id`, `menu_item_name`, `menu_item_url`, `menu_item_parent`, `menu_item_activ`) VALUES
+(1, 1, 'Главная', '?', 0, 1),
+(2, 1, 'Новости', '?ctrl=news&action=News&page=1', 0, 1),
+(3, 1, 'О нас', '?ctrl=page&action=Page&id=1', 0, 1),
+(4, 2, 'Высшее образование', '?ctrl=wisdom&action=WisdomType&type=1', 0, 1),
+(5, 2, 'Первое высшее образование', '?ctrl=wisdom&action=WisdomType&type=1&subtype=1', 4, 1),
+(6, 2, 'Переподготовка', '?ctrl=wisdom&action=WisdomType&type=1&subtype=2', 4, 1),
+(7, 2, 'Сокращённое обучение', '?ctrl=wisdom&action=WisdomType&type=1&subtype=3', 4, 1),
+(8, 2, 'Курсы', '?ctrl=wisdom&action=WisdomType&type=2', 0, 1),
+(9, 2, 'Сертифицированные', '?ctrl=wisdom&action=WisdomType&type=2&subtype=1', 8, 1),
+(10, 2, 'Не сертифицированные', '?ctrl=wisdom&action=WisdomType&type=2&subtype=2', 8, 1),
+(11, 2, 'Семинары', '?ctrl=wisdom&action=WisdomType&type=3', 0, 1),
+(12, 2, 'Мастер-классы', '?ctrl=wisdom&action=WisdomType&type=3&subtype=1', 11, 1),
+(13, 2, 'Доклады', '?ctrl=wisdom&action=WisdomType&type=3&subtype=2', 11, 1),
+(14, 2, 'Библиотека', '#', 0, 1),
+(15, 2, 'Статьи', '?ctrl=wisdom&action=WisdomType&type=3&subtype=3', 11, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `news`
+--
+
+CREATE TABLE IF NOT EXISTS `news` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `news_header` varchar(90) NOT NULL,
+  `news_body` text NOT NULL,
+  `news_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `news_img` varchar(80) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `news_activ` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+
+--
+-- Дамп данных таблицы `news`
+--
+
+INSERT INTO `news` (`id`, `news_header`, `news_body`, `news_date`, `news_img`, `user_id`, `news_activ`) VALUES
+(1, 'Вчера во время проведения разведоперации наша группа подверглась нападению неизвестного пр', 'Сегодня во время проведения разведоперации наша группа подверглась нападению неизвестного противника в камуфляжной форме Алиенов.', '2015-11-30 12:44:27', 'images/photoTest.png', 1, 1),
+(2, 'Phasellus blandit nisl ac commodo aliquam.', 'Вчера во время проведения разведоперации наша группа подверглась нападению неизвестного противника в камуфляжной форме Алиенов.', '2015-11-30 14:40:27', 'images/IMG_20151228_120826.jpg', 1, 1),
+(3, 'Praesent semper dui condimentum, auctor velit vitae, sagittis sapien.', 'Вчера во время проведения разведоперации наша группа подверглась нападению неизвестного противника в камуфляжной форме Алиенов.', '2015-11-30 14:44:56', 'images/20151228_120827.jpg', 1, 1),
+(4, 'Sed in odio ac odio elementum ullamcorper et quis metus.', 'Вчера во время проведения разведоперации наша группа подверглась нападению неизвестного противника в камуфляжной форме Алиенов.', '2015-11-30 14:45:09', 'images/20151228_115024.jpg', 1, 1),
+(5, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Вчера во время проведения разведоперации наша группа подверглась нападению неизвестного противника в камуфляжной форме Алиенов.', '2015-11-30 14:45:40', 'images/12_100229_1_96372.jpg', 1, 1),
+(6, 'Sed molestie quam id sapien consequat, pellentesque fringilla arcu commodo.', 'Вчера во время проведения разведоперации наша группа подверглась нападению неизвестного противника в камуфляжной форме Алиенов.', '2015-11-30 14:45:50', 'images/12_100229_1_96370.jpg', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `page`
+--
+
+CREATE TABLE IF NOT EXISTS `page` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` char(40) NOT NULL,
+  `body` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Дамп данных таблицы `page`
+--
+
+INSERT INTO `page` (`id`, `title`, `body`) VALUES
+(1, 'О нас', '<h1>Мы молодцы</h1>'),
+(2, 'Контакты', '<h1>Садовая 32Б,кв 50</h1>\r\n<h2>Спросить Коровьева<h2>');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_login` char(40) NOT NULL,
+  `user_password` char(40) NOT NULL,
+  `user_status` char(40) NOT NULL,
+  `user_block` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Дамп данных таблицы `user`
+--
+
+INSERT INTO `user` (`id`, `user_login`, `user_password`, `user_status`, `user_block`) VALUES
+(1, 'admin', '123', 'admin', 0),
+(2, 'vasea', '123', 'student', 0),
+(3, 'alla', '123', 'teacher', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user_data`
+--
+
+CREATE TABLE IF NOT EXISTS `user_data` (
+  `user_data_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_data_name` char(40) NOT NULL,
+  `user_data_andername` char(40) NOT NULL,
+  `user_data_surname` char(40) NOT NULL,
+  `user_data_email` char(80) NOT NULL,
+  `user_data_land` char(40) NOT NULL,
+  `user_data_sity` char(40) NOT NULL,
+  `user_data_phone` int(20) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_data_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Дамп данных таблицы `user_data`
+--
+
+INSERT INTO `user_data` (`user_data_id`, `user_data_name`, `user_data_andername`, `user_data_surname`, `user_data_email`, `user_data_land`, `user_data_sity`, `user_data_phone`, `user_id`) VALUES
+(1, 'Василий', 'Васильевич', 'Василевский', 'vasea@mail.ru', 'Беларусь', 'Минск', 222, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user_wisdom`
+--
+
+CREATE TABLE IF NOT EXISTS `user_wisdom` (
+  `user_wisdom_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `wisdom_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_wisdom_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `wcategory`
+--
+
+CREATE TABLE IF NOT EXISTS `wcategory` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` char(80) NOT NULL,
+  `wsubtype_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+
+--
+-- Дамп данных таблицы `wcategory`
+--
+
+INSERT INTO `wcategory` (`id`, `name`, `wsubtype_id`) VALUES
+(1, 'Дизайн', 1),
+(2, 'Программирование', 2),
+(3, 'Бухгалтерия', 4),
+(4, 'Программирование', 4),
+(5, 'Дизайн', 5),
+(6, 'Программирование', 7);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `wisdom`
+--
+
+CREATE TABLE IF NOT EXISTS `wisdom` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `wsubcategory_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=15 ;
+
+--
+-- Дамп данных таблицы `wisdom`
+--
+
+INSERT INTO `wisdom` (`id`, `name`, `wsubcategory_id`) VALUES
+(1, 'Вёрстка', 1),
+(2, 'JS для начинающих', 5),
+(3, 'Инженер', 7),
+(4, 'Дизайн', 4),
+(5, 'Безопастность', 9),
+(6, 'Серверное программирование', 3),
+(7, 'Как писать на PHP и не сойти с ума', 5),
+(8, 'Задачи 1С', 2),
+(9, 'Ангулар', 10),
+(10, 'Sed ultricies tortor', 8),
+(11, 'blandit suscipit sed vitae eros.', 2),
+(12, 'Mauris semper', 10),
+(13, 'Donec nec nibh nec', 1),
+(14, 'RedBean как средство для суицида', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `wsubcategory`
+--
+
+CREATE TABLE IF NOT EXISTS `wsubcategory` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` char(80) NOT NULL,
+  `wcategory_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
+
+--
+-- Дамп данных таблицы `wsubcategory`
+--
+
+INSERT INTO `wsubcategory` (`id`, `name`, `wcategory_id`) VALUES
+(1, 'Вёрстка', 1),
+(2, 'Колористика', 5),
+(3, 'Серверное программирование', 2),
+(4, 'Инженер', 4),
+(5, '1С', 4),
+(6, 'Торговый баланс', 5),
+(7, 'JS для начинающих', 2),
+(8, 'Ангулар', 5),
+(9, 'Юзабилити', 2),
+(10, 'Веб-вёрстка', 3),
+(11, 'RedBean как средство для суицида', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `wsubtype`
+--
+
+CREATE TABLE IF NOT EXISTS `wsubtype` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `wtype_id` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_foreignkey_wsubtype_wtype` (`wtype_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
+
+--
+-- Дамп данных таблицы `wsubtype`
+--
+
+INSERT INTO `wsubtype` (`id`, `name`, `wtype_id`) VALUES
+(1, 'Первое высшее', 1),
+(2, 'Переподготовка', 1),
+(3, 'Сокращённое обучение', 1),
+(4, 'Сертифицированные', 2),
+(5, 'Не сертифицированные', 2),
+(6, 'Мастер-классы', 3),
+(7, 'Доклады', 3),
+(8, 'Статьи', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `wtype`
+--
+
+CREATE TABLE IF NOT EXISTS `wtype` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+
+--
+-- Дамп данных таблицы `wtype`
+--
+
+INSERT INTO `wtype` (`id`, `name`) VALUES
+(1, 'Высшее образование'),
+(2, 'Курсы'),
+(3, 'Семинары');
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `wsubtype`
+--
+ALTER TABLE `wsubtype`
+  ADD CONSTRAINT `c_fk_wsubtype_wtype_id` FOREIGN KEY (`wtype_id`) REFERENCES `wtype` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
