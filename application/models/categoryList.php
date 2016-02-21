cd34d4411a3fa1057c90a98569ce4a9ead3d06c8
Jluct <panaev02@gmail.com>
2016-02-21 21:26:29 +0300

    Bulding menu cat

--- application/models/categoryList.php
+++ application/models/categoryList.php
@@ -9,31 +9,42 @@
 class categoryList
 {
 
-    static function categorMenu($int)
+    static function categorMenu($int, $cat='', $subcat='')
     {
         db_connect::connect();
-        $data = R::findAll('wsubtype', " WHERE wtype_id = :id", ['id' => $int]);
-        $out = "<div class=\"btn-group-vertical\" role=\"group\" aria-label=\"...\">";
-        foreach ($data as $categor)
-        {
-            $item = "<div class=\"btn-group\" role=\"group\">
-    <button type=\"button\" class=\"btn btn-default dropdown-toggle\"
-    data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
-      " . $categor->name . "
-      <span class=\"caret\"></span>
-    </button>
-    <ul class=\"dropdown-menu\">";
+        $data = R::getAll("SELECT
+	    wtype.id AS type_id,
+        wsubtype.id AS wsubtype_id,
+        wcategory.id AS category_id,
+        wcategory.name AS category_name,
+        wsubcategory.id AS subcategory_id,
+        wsubcategory.name AS subcategory_name
+        FROM wtype
+        LEFT JOIN wsubtype ON wsubtype.wtype_id = wtype.id
+        LEFT JOIN wcategory ON wcategory.wsubtype_id = wsubtype.id
+        LEFT JOIN wsubcategory ON wsubcategory.wcategory_id = wcategory.id
+        WHERE wtype.id = :id", ['id' => $int]);
 
-            foreach ($categor->ownWcategoryList as $subcateg)
-            {
-
-                $item .= "<li><a href=\"#\">" . $subcateg->name . "</a></li>";
+        $out="<ul>";
+            foreach($data as $item){
+                $out.="<li>".$item['subcategory_name']."</li>";
             }
-
-            $item .= "</ul></div>";
-            $out .= $item;
-        }
-        $out .= "</div>";
+        $out.="</ul>";
         return $out;
     }
 }
+
+/*
+ * SELECT
+	wtype.id AS type_id,
+        wsubtype.id AS wsubtype_id,
+        wcategory.id AS category_id,
+        wcategory.name AS category_name,
+        wsubcategory.id AS subcategory_id,
+        wsubcategory.name AS subcategory_name
+        FROM wtype
+        LEFT JOIN wsubtype ON wsubtype.wtype_id = wtype.id
+        LEFT JOIN wcategory ON wcategory.wsubtype_id = wsubtype.id
+        LEFT JOIN wsubcategory ON wsubcategory.wcategory_id = wcategory.id
+        WHERE wtype.id = 1
+ */
